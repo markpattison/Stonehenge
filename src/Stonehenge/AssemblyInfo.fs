8cecb09e2c8ff769f8c7968c4d53265b7e63def0
markpattison <mr.pattison@gmail.com>
2019-04-01 20:40:09 +0100

    correct window title

--- src/Stonehenge/AssemblyInfo.fs
+++ src/Stonehenge/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("fsharp-MonoGame")>]
+[<assembly: AssemblyTitle("Stonehenge")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("fsharp-MonoGame")>]
+[<assembly: AssemblyProduct("Stonehenge")>]
 [<assembly: AssemblyCopyright("Copyright ©  2018")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
