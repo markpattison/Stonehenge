3ab1636758d18558c0af6ec5f550f1ced6453833
Mark Pattison <mpattison@hymans.co.uk>
2019-04-19 10:25:22 +0100

    stones

--- src/Stonehenge/StoneList.fs
+++ src/Stonehenge/StoneList.fs
@@ -4,6 +4,34 @@ open Stonehenge.Stone
 
 let stones =
     [
-        { Radius = 10.0f; Rotation = 0.0f; Orientation = Upright }
-        { Radius = 10.0f; Rotation = 0.0f; Orientation = Flat }
+        { Radius = 10.0f; Rotation = 360.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 20.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 30.0f; Orientation = Flat }
+        { Radius = 10.0f; Rotation = 40.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 58.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 82.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 100.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 118.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 140.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 150.0f; Orientation = Flat }
+        { Radius = 10.0f; Rotation = 160.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 180.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 200.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 222.0f; Orientation = Upright }
+        
+        { Radius = 10.0f; Rotation = 240.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 250.0f; Orientation = Flat }
+        { Radius = 10.0f; Rotation = 260.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 280.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 298.0f; Orientation = Upright }
+
+        { Radius = 10.0f; Rotation = 320.0f; Orientation = Upright }
+        { Radius = 10.0f; Rotation = 330.0f; Orientation = Flat }
+        { Radius = 10.0f; Rotation = 340.0f; Orientation = Upright }
     ]
